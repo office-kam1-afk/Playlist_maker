9a4f98d202cda4463a8bff65d8cfbf2bff8b7e19
nemo <office-kam1@yandex.ru>
2026-04-20 01:38:44 +0300

    Merge pull request #4 from office-kam1-afk/dev_spri9-1-1-1
    Dev spri9 1 1 1

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -13,18 +13,19 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val trackImage: ImageView = itemView.findViewById(R.id.track_image)
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
     private val artistName: TextView = itemView.findViewById(R.id.artist_name)
-    private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+   // private val trackTime: TextView? = itemView.findViewById(R.id.track_time)
 
     fun bind(track: Track) {
         trackName.text = track.trackName
-        artistName.text = track.artistName
-        trackTime.text = track.trackTime
+        artistName.text = "${track.artistName} • ${track.trackTime}"
+       // trackTime.visibility = View.GONE
+        //trackTime.text = track.trackTime
 
         Glide.with(itemView.context)
             .load(track.artworkUrl100)
-            .diskCacheStrategy(DiskCacheStrategy.ALL)
-            .placeholder(R.drawable.ic_music_empty)
-            .error(R.drawable.ic_music_empty)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .placeholder(R.drawable.ic_placeholder)
+            .error(R.drawable.ic_placeholder)
             .centerCrop()
             .apply(RequestOptions().transform(RoundedCorners(8)))
             .into(trackImage)
